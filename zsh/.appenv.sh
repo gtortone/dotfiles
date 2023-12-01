20056e1c29d9f4e82c8f2bc75e8c9a31bc46a95a
Gennaro Tortone <gennaro.tortone@na.infn.it>
2023-12-01 09:23:44 +0100

    disable pyenv

--- zsh/.appenv.sh
+++ zsh/.appenv.sh
@@ -80,6 +80,6 @@ export PATH="$PATH:${JAVA_HOME}/bin"
 export PATH="$PATH:/opt/go/bin"
 
 # pyenv
-export PYENV_ROOT="$HOME/.pyenv"
-command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
+#export PYENV_ROOT="$HOME/.pyenv"
+#command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+#eval "$(pyenv init -)"
